66a2bef0aa5fe9e6df7cf21d5569c26d44d2cbcf
ngeshlew <ngugilewis@gmail.com>
2015-12-31 12:08:03 +0300

    Update Files

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myToDoAppAngularApp
+ * @description
+ * # myToDoAppAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myToDoAppAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myToDoAppAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myToDoAppAngularApp
+ */
+angular.module('myToDoAppAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
